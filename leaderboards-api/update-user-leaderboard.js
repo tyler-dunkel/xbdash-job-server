2b9cefef71fed5ba5d1f8da35dabc64aa3c95271
kg187 <kguirao87@gmail.com>
2016-04-05 02:49:56 -0400

    working on leaderboards and news migration

--- leaderboards-api/update-user-leaderboard.js
+++ leaderboards-api/update-user-leaderboard.js
@@ -26,49 +26,41 @@ module.exports = function(user, topCallback) {
 			function(cb) {
 				async.parallel([
 					function(callback) {
-						//console.log('calling user daily count');
 						dailyCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling completed games count');
 						completedGameCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling completed achievements count');
 						completedAchievementCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling total achievements count');
 						totalAchievementCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling common achievements count');
 						commonAchievementsCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling rare achievements count');
 						rareAchievementsCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling epic achievements count');
 						epicAchievementsCount(user, function() {
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling legendary achievements count');
 						legendaryAchievementsCount(user, function() {
 							callback();
 						});
@@ -86,63 +78,54 @@ module.exports = function(user, topCallback) {
 			function(cb) {
 				async.parallel([
 					function(callback) {
-						console.log('calling overall ranks');
 						updateRanks.overallRank(function() {
 							console.log('overall ranks callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling user daily ranks');
 						updateRanks.dailyRank(function() {
 							console.log('daily callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling completed games rank');
 						updateRanks.completedGamesRank(function() {
 							console.log('completed games callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling completed achievements rank');
 						updateRanks.completedAchievementsRank(function() {
 							console.log('completed achievements callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling total achievements rank');
 						updateRanks.totalAchievementsRank(function() {
 							console.log('total achievements callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling common achievements rank');
 						updateRanks.commonAchievementsRank(function() {
 							console.log('common achievements callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						console.log('calling rare achievements rank');
 						updateRanks.rareAchievementsRank(function() {
 							console.log('rare achievements callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling epic achievements rank');
 						updateRanks.epicAchievementsRank(function() {
 							console.log('epic achievements callback fired');
 							callback();
 						});
 					},
 					function(callback) {
-						//console.log('calling legendary achievements rank');
 						updateRanks.legendaryAchievementsRank(function() {
 							console.log('legendary achievements callback fired');
 							callback();
