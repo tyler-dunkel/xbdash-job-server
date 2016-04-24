198ba35c575701599cbc8bf5212a1b9fc59d231e
kg187 <kguirao87@gmail.com>
2016-04-23 23:47:33 -0400

    changes

--- jobs.js
+++ jobs.js
@@ -6,88 +6,14 @@ var later = require('later');
 var workers = require('./workers.js');
 var db = require('./db.js');
 
-// var ddp = new DDP({
-// 	host: 'beta.xbdash.com',
-// 	port: 3000,
-// 	ssl: true,
-// 	autoReconnect : true,
-// 	autoReconnectTimer : 500,
-// 	ddpVersion : '1',
-// 	url: 'wss://beta.xbdash.com/websocket',
-// 	use_ejson: true
-// });
-
-// Job.setDDP(ddp);
-
-// ddp.connect(function (err, wasReconnect) {
-// 	if (err) throw err;
-// 	if (wasReconnect) {
-// 		console.log('connection reestablished');
-// 		jobRunToCompleted(function(err, res) {
-// 			if (err) {
-// 				console.log('error sending welcome email');
-// 				return;
-// 			}
-// 			console.log('all jobs and users set back to default');
-// 		});
-// 	}
-// 	DDPlogin(ddp, {
-// 		env: 'METEOR_TOKEN',
-// 		method: 'email',
-// 		account: 'tyler.dunkel@gmail.com',
-// 		pass: 'Tjd11034',
-// 		retry: 5
-// 	}, function (err, token) {
-// 		if (err) {
-// 			db.close();
-// 			throw err;
-// 		}
-
-// 		console.log('connected to xbdash prod');
-
-// 		jobRunToCompleted(function(err, res) {
-// 			if (err) {
-// 				console.log('error sending welcome email');
-// 				return;
-// 			}
-// 			console.log('all jobs and users set back to default');
-// 		});
-
-// 		var dirtyUserStatsJob = new Job('xbdjobscollection', 'dirtyUserStatsJob', {})
-// 			.priority('normal')
-// 			.repeat({
-// 				schedule: later.parse.text('every 5 mins')
-// 			})
-// 			.save(function (err, result) {
-// 				if (err) return;
-// 				if (!err && result) {
-// 					console.log('dirty user stats job saved with ID: ' + result);
-// 				}
-// 			});
-
-// 		var clearDailyRanksJob = new Job('xbdjobscollection', 'clearDailyRanksJob', {})
-// 			.priority('normal')
-// 			.repeat({
-//				schedule: later.parse.text('at 12:00 am starting on the 24th day of April in 2016')
-// 			})
-// 			.save(function (err, result) {
-// 				if (err) return;
-// 				if (!err && result) {
-// 					console.log('clear daily ranks job saved with ID: ' + result);
-// 				}
-// 			});
-
-// 		var profileBuilderWorker = Job.processJobs('xbdjobscollection', 'buildUserProfileJob', workers.profileBuilder);
-// 		var dirtyUpdateUserStatsWorker = Job.processJobs('xbdjobscollection', 'dirtyUserStatsJob', { workTimeout: 600000 }, workers.dirtyUpdateUserStats);
-// 		var clearDailyRanksWorker = Job.processJobs('xbdjobscollection', 'clearDailyRanksJob', { workTimeout: 600000 }, workers.clearDailyRanks);
-// 	});
-// });
-
-
-
 var ddp = new DDP({
-	host: 'localhost',
+	host: 'beta.xbdash.com',
 	port: 3000,
+	ssl: true,
+	autoReconnect : true,
+	autoReconnectTimer : 500,
+	ddpVersion : '1',
+	url: 'wss://beta.xbdash.com/websocket',
 	use_ejson: true
 });
 
@@ -108,8 +34,8 @@ ddp.connect(function (err, wasReconnect) {
 	DDPlogin(ddp, {
 		env: 'METEOR_TOKEN',
 		method: 'email',
-		account: 'kguirao87@gmail.com',
-		pass: '121212',
+		account: 'tyler.dunkel@gmail.com',
+		pass: 'Tjd11034',
 		retry: 5
 	}, function (err, token) {
 		if (err) {
@@ -117,7 +43,7 @@ ddp.connect(function (err, wasReconnect) {
 			throw err;
 		}
 
-		console.log('connected to xbdash');
+		console.log('connected to xbdash prod');
 
 		jobRunToCompleted(function(err, res) {
 			if (err) {
@@ -130,7 +56,7 @@ ddp.connect(function (err, wasReconnect) {
 		var dirtyUserStatsJob = new Job('xbdjobscollection', 'dirtyUserStatsJob', {})
 			.priority('normal')
 			.repeat({
-				schedule: later.parse.text('every 2 mins')
+				schedule: later.parse.text('every 5 mins')
 			})
 			.save(function (err, result) {
 				if (err) return;
@@ -156,3 +82,77 @@ ddp.connect(function (err, wasReconnect) {
 		var clearDailyRanksWorker = Job.processJobs('xbdjobscollection', 'clearDailyRanksJob', { workTimeout: 600000 }, workers.clearDailyRanks);
 	});
 });
+
+
+
+// var ddp = new DDP({
+// 	host: 'localhost',
+// 	port: 3000,
+// 	use_ejson: true
+// });
+
+// Job.setDDP(ddp);
+
+// ddp.connect(function (err, wasReconnect) {
+// 	if (err) throw err;
+// 	if (wasReconnect) {
+// 		console.log('connection reestablished');
+// 		jobRunToCompleted(function(err, res) {
+// 			if (err) {
+// 				console.log('error sending welcome email');
+// 				return;
+// 			}
+// 			console.log('all jobs and users set back to default');
+// 		});
+// 	}
+// 	DDPlogin(ddp, {
+// 		env: 'METEOR_TOKEN',
+// 		method: 'email',
+// 		account: 'kguirao87@gmail.com',
+// 		pass: '121212',
+// 		retry: 5
+// 	}, function (err, token) {
+// 		if (err) {
+// 			db.close();
+// 			throw err;
+// 		}
+
+// 		console.log('connected to xbdash');
+
+// 		jobRunToCompleted(function(err, res) {
+// 			if (err) {
+// 				console.log('error sending welcome email');
+// 				return;
+// 			}
+// 			console.log('all jobs and users set back to default');
+// 		});
+
+// 		var dirtyUserStatsJob = new Job('xbdjobscollection', 'dirtyUserStatsJob', {})
+// 			.priority('normal')
+// 			.repeat({
+// 				schedule: later.parse.text('every 2 mins')
+// 			})
+// 			.save(function (err, result) {
+// 				if (err) return;
+// 				if (!err && result) {
+// 					console.log('dirty user stats job saved with ID: ' + result);
+// 				}
+// 			});
+
+// 		var clearDailyRanksJob = new Job('xbdjobscollection', 'clearDailyRanksJob', {})
+// 			.priority('normal')
+// 			.repeat({
+// 				schedule: later.parse.text('at 12:00 am starting on the 24th day of April in 2016')
+// 			})
+// 			.save(function (err, result) {
+// 				if (err) return;
+// 				if (!err && result) {
+// 					console.log('clear daily ranks job saved with ID: ' + result);
+// 				}
+// 			});
+
+// 		var profileBuilderWorker = Job.processJobs('xbdjobscollection', 'buildUserProfileJob', workers.profileBuilder);
+// 		var dirtyUpdateUserStatsWorker = Job.processJobs('xbdjobscollection', 'dirtyUserStatsJob', { workTimeout: 600000 }, workers.dirtyUpdateUserStats);
+// 		var clearDailyRanksWorker = Job.processJobs('xbdjobscollection', 'clearDailyRanksJob', { workTimeout: 600000 }, workers.clearDailyRanks);
+// 	});
+// });
