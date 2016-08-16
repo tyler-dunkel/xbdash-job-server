8d45f7b0ac0503d91283d976be4fd32fca7d6da8
kg187 <kguirao87@gmail.com>
2016-08-16 01:32:11 -0400

    updated js

--- xbox-profile-update.js
+++ xbox-profile-update.js
@@ -8,18 +8,18 @@
 
 var db = require('./db.js');
 var users = db.collection('users');
-var userId = 'kFhehHMFrp9GF7B4M';
+var userId = 'YP5iXtDHda5E7ZMXH';
 var gtSlugify = require('./gamertag-slugify.js');
 
 users.findOne({ _id: userId }, function(err, user) {
 	console.log(user.xboxProfile);
-	if (user.gamercard) {
-		gtSlugify.gamercardSlugify(userId, user.gamercard, function(err) {
-			console.log(userId + " " + user.gamercard);
-		});
-	} else {
+	// if (user.gamercard) {
+	// 	gtSlugify.gamercardSlugify(userId, user.gamercard, function(err) {
+	// 		console.log(userId + " " + user.gamercard);
+	// 	});
+	// } else {
 		gtSlugify.xboxProfileSlugify(userId, user.xboxProfile, function(err) {
 			console.log(userId + " " + user.xboxProfile);
 		});
-	}
+	// }
 });
