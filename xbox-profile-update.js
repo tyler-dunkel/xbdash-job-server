cbc82b38bd4ac907234d33ac2d67345663b6eab1
kg187 <kguirao87@gmail.com>
2016-08-16 02:59:00 -0400

    added proper gamertag-slugify syntax

--- xbox-profile-update.js
+++ xbox-profile-update.js
@@ -1,25 +1,26 @@
-// var xboxApiObject = require('./xbox-api.js');
-// var xboxApiCaller = require('./xbox-api-caller.js');
-// var db = require('./db.js');
-
-// xboxApiObject.updateXboxProfile(userId, function(err) {
-// 	console.log(userId + 'is done');
-// });
-
+var xboxApiObject = require('./xbox-api.js');
+var xboxApiCaller = require('./xbox-api-caller.js');
 var db = require('./db.js');
-var users = db.collection('users');
 var userId = 'YP5iXtDHda5E7ZMXH';
-var gtSlugify = require('./gamertag-slugify.js');
 
-users.findOne({ _id: userId }, function(err, user) {
-	console.log(user.xboxProfile);
-	// if (user.gamercard) {
-	// 	gtSlugify.gamercardSlugify(userId, user.gamercard, function(err) {
-	// 		console.log(userId + " " + user.gamercard);
-	// 	});
-	// } else {
-		gtSlugify.xboxProfileSlugify(userId, user.xboxProfile, function(err) {
-			console.log(userId + " " + user.xboxProfile);
-		});
-	// }
+xboxApiObject.updateXboxProfile(userId, function(err) {
+	console.log(userId + 'is done');
 });
+
+// var db = require('./db.js');
+// var users = db.collection('users');
+// var userId = 'YP5iXtDHda5E7ZMXH';
+// var gtSlugify = require('./gamertag-slugify.js');
+
+// users.findOne({ _id: userId }, function(err, user) {
+// 	console.log(user.xboxProfile);
+// 	if (user.gamercard) {
+// 		gtSlugify.gamercardSlugify(userId, user.gamercard, function(err) {
+// 			console.log(userId + " " + user.gamercard);
+// 		});
+// 	} else {
+// 		gtSlugify.xboxProfileSlugify(userId, user.xboxProfile, function(err) {
+// 			console.log(userId + " " + user.xboxProfile);
+// 		});
+// 	}
+// });
