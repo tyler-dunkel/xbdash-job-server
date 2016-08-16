9b23cf9bbb38fd9a4270c6d550ad6ac6d3f47d3c
kg187 <kguirao87@gmail.com>
2016-08-16 01:34:04 -0400

    updated js

--- gamertag-slugify.js
+++ gamertag-slugify.js
@@ -50,3 +50,26 @@ module.exports = {
 	gamercardSlugify: gamercardSlugify,
 	xboxProfileSlugify: xboxProfileSlugify
 }
+
+// original slugify code
+// var tr = require('transliteration');
+// var slugify = require('transliteration').slugify;
+// var db = require('./db.js');
+
+// module.exports = function(userId, gamercard, cb) {
+// 	var users = db.collection('users');
+// 	users.findOne({ _id: userId }, function(err, user) {
+// 		if (user.gamertagSlug) {
+// 			cb && cb();
+// 			return;
+// 		}
+// 		console.log(gamercard.gamertag);
+// 		var gamertagSlug = slugify(gamercard.gamertag, { lowercase: true });
+// 		users.update({_id: userId}, {$set: {gamertagSlug: gamertagSlug}}, function(err) {
+// 			if (err) {
+// 				console.log(err);
+// 			}
+// 			cb && cb();
+// 		});
+// 	});
+// }
