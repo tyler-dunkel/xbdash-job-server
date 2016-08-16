aaa35059e609d4e29790de88ba6ff0acfbe855a0
kg187 <kguirao87@gmail.com>
2016-08-16 01:11:43 -0400

    added xboxProfile slugify

--- db.js
+++ db.js
@@ -1,4 +1,3 @@
-
 if (process.env.STATE === 'prod') {
 	var meteorUrl = 'mongodb://xbadmin:tda88f552e87k15g@capital.4.mongolayer.com:10130,capital.5.mongolayer.com:10130/xbdash-production?replicaSet=set-569077792b73d794a800039f';
 } else {
--- gamertag-slugify.js
+++ gamertag-slugify.js
@@ -2,20 +2,51 @@ var tr = require('transliteration');
 var slugify = require('transliteration').slugify;
 var db = require('./db.js');
 
-module.exports = function(userId, gamercard, cb) {
+var gamercardSlugify = function(userId, gamercard, cb) {
 	var users = db.collection('users');
 	users.findOne({ _id: userId }, function(err, user) {
 		if (user.gamertagSlug) {
 			cb && cb();
 			return;
 		}
+
 		console.log(gamercard.gamertag);
-		var gamertagSlug = slugify(gamercard.gamertag, { lowercase: true });
-		users.update({_id: userId}, {$set: {gamertagSlug: gamertagSlug}}, function(err) {
+
+		var userGamertag = user.gamercard.gamertag;
+		var gamertagSlug = slugify(userGamertag, { lowercase: true });
+
+		users.update({ _id: userId }, { $set: { gamertagSlug: gamertagSlug } }, function(err) {
 			if (err) {
 				console.log(err);
 			}
 			cb && cb();
 		});
 	});
+}
+
+var xboxProfileSlugify = function(userId, xboxProfile, cb) {
+	var users = db.collection('users');
+	users.findOne({ _id: userId }, function(err, user) {
+		if (user.gamertagSlug) {
+			cb && cb();
+			return;
+		}
+
+		console.log(xboxProfile.gamertag);
+
+		var userGamertag = user.xboxProfile.gamertag;
+		var gamertagSlug = slugify(userGamertag, { lowercase: true });
+
+		users.update({ _id: userId }, { $set: { gamertagSlug: gamertagSlug } }, function(err) {
+			if (err) {
+				console.log(err);
+			}
+			cb && cb();
+		});
+	});
+}
+
+module.exports = {
+	gamercardSlugify: gamercardSlugify,
+	xboxProfileSlugify: xboxProfileSlugify
 }
