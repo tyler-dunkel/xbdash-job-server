d57f07fbf4f008cfd339d474bbe500c8e5a09a9f
kg187 <kguirao87@gmail.com>
2016-11-09 02:28:50 -0500

    complete jobs fix

--- completeJobs.js
+++ completeJobs.js
@@ -3,11 +3,3 @@ var db = require('./db.js');
 db.collection('xbdjobscollection.jobs').remove({ status: 'completed' }, function(err, result) {
 	console.log('removed completed docs');
 });
-
-db.collection('xbdjobscollection.jobs').remove({ status: 'ready', type: "updateGameClips" }, function(err, result) {
-	console.log('removed game clip docs');
-});
-
-db.collection('xbdjobscollection.jobs').remove({ status: 'failed' }, function(err, result) {
-	console.log('removed failed jobs');
-});
