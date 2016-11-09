b31c0976544f999972777b96579fe0935f53f6f5
kg187 <kguirao87@gmail.com>
2016-11-09 02:10:39 -0500

    jobs server fix

--- completeJobs.js
+++ completeJobs.js
@@ -3,7 +3,3 @@ var db = require('./db.js');
 db.collection('xbdjobscollection.jobs').remove({ status: 'completed' }, function(err, result) {
 	console.log('removed completed docs');
 });
-
-db.collection('xbdjobscollection.jobs').remove({ status: 'failed' }, function(err, result) {
-	console.log('removed failed jobs');
-});
